14ee71c51395d1f507571b53b5e10bb31de780d4
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-10-13 21:24:01 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace DateTimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datum = new System.Windows.Forms.DateTimePicker();
+            this.vrijeme = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rezervacija = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // datum
+            // 
+            this.datum.Location = new System.Drawing.Point(226, 33);
+            this.datum.Name = "datum";
+            this.datum.Size = new System.Drawing.Size(96, 20);
+            this.datum.TabIndex = 0;
+            // 
+            // vrijeme
+            // 
+            this.vrijeme.Location = new System.Drawing.Point(226, 84);
+            this.vrijeme.Name = "vrijeme";
+            this.vrijeme.Size = new System.Drawing.Size(96, 20);
+            this.vrijeme.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Upišite datum leta:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Upišite vrijeme polaska:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(50, 145);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(272, 136);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // rezervacija
+            // 
+            this.rezervacija.Location = new System.Drawing.Point(103, 340);
+            this.rezervacija.Name = "rezervacija";
+            this.rezervacija.Size = new System.Drawing.Size(137, 28);
+            this.rezervacija.TabIndex = 5;
+            this.rezervacija.Text = "Rezervacija";
+            this.rezervacija.UseVisualStyleBackColor = true;
+            this.rezervacija.Click += new System.EventHandler(this.rezervacija_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 450);
+            this.Controls.Add(this.rezervacija);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vrijeme);
+            this.Controls.Add(this.datum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker datum;
+        private System.Windows.Forms.DateTimePicker vrijeme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button rezervacija;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            datum.Format = DateTimePickerFormat.Short;
+            vrijeme.Format = DateTimePickerFormat.Time;
+            vrijeme.ShowUpDown = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rezervacija_Click(object sender, EventArgs e)
+        {
+            string rezervacijaPoruka = $"Vaša rezervacija je potvrđena za {datum.Value.ToShortDateString()} u {vrijeme.Value.ToShortTimeString()}. Hvala!";
+            richTextBox1.Text = rezervacijaPoruka;
+        }
+    }
+}
